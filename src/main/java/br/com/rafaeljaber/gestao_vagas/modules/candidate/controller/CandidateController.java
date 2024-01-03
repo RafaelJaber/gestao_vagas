38a9f846f2629d3b98fc9d8ced0412b9486bada6
Rafael Jáber <rafael.jaber@hotmail.com>
2024-01-03 18:24:50 -0300

    Adicionei anotações do Swagger e perfil do candidato
    A classe ProfileCandidateResponseDTO foi aprimorada com anotações de esquema do Swagger, fornecendo valores de exemplo para cada campo. No CandidateController, um novo endpoint foi detalhado, informando suas operações e possíveis respostas usando as anotações Operation, Tag, ApiResponse e SecurityRequirement do Swagger. Essas mudanças ajudam a fornecer documentação detalhada da API.

--- src/main/java/br/com/rafaeljaber/gestao_vagas/modules/candidate/controller/CandidateController.java
+++ src/main/java/br/com/rafaeljaber/gestao_vagas/modules/candidate/controller/CandidateController.java
@@ -1,5 +1,6 @@
 package br.com.rafaeljaber.gestao_vagas.modules.candidate.controller;
 
+import br.com.rafaeljaber.gestao_vagas.modules.candidate.dto.ProfileCandidateResponseDTO;
 import br.com.rafaeljaber.gestao_vagas.modules.candidate.entities.CandidateEntity;
 import br.com.rafaeljaber.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import br.com.rafaeljaber.gestao_vagas.modules.candidate.useCases.ListAllJobsByFilterUseCase;
@@ -49,6 +50,21 @@ public class CandidateController {
 
     @GetMapping("/")
     @PreAuthorize("hasRole('CANDIDATE')")
+    @Tag(name = "Candidato", description = "Informações do candidato")
+    @Operation(summary = "Perfil do candidato", description = "Essa função é responsável por buscar as informações do perfil do candidato")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(
+                            schema = @Schema(implementation = ProfileCandidateResponseDTO.class)
+                    )
+            }),
+            @ApiResponse(responseCode = "400", content = {
+                    @Content(
+                            schema = @Schema(implementation = String.class)
+                    )
+            })
+    })
+    @SecurityRequirement(name = "jwt_auth")
     public ResponseEntity<Object> get(HttpServletRequest request) {
         var idCandidate = request.getAttribute("candidate_id");
 
--- src/main/java/br/com/rafaeljaber/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
+++ src/main/java/br/com/rafaeljaber/gestao_vagas/modules/candidate/dto/ProfileCandidateResponseDTO.java
@@ -1,5 +1,6 @@
 package br.com.rafaeljaber.gestao_vagas.modules.candidate.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,9 +14,17 @@ import java.util.UUID;
 @AllArgsConstructor
 public class ProfileCandidateResponseDTO {
 
+    @Schema(example =  "Desenvolvedor Java")
     private String description;
+
+    @Schema(example =  "john_doe")
     private String username;
+
+    @Schema(example =  "john.doe@example.com")
     private String email;
+
+    @Schema(example =  "John Doe")
     private String name;
+
     private UUID id;
 }
