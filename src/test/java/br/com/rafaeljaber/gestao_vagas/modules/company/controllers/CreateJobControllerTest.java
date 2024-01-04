2b42b8bb7513da9f1965cf656c78caf1b44fd27c
Rafael Jáber <rafael.jaber@hotmail.com>
2024-01-04 13:14:34 -0300

    Configurado o banco de dados H2 em memória e aprimore CompanyEntity
    Configurei o banco de dados na memória H2 e adicionei sua dependência em pom.xml para fins de teste. Aprimorada a classe CompanyEntity com anotações Lombok para reduzir o código padrão. Além disso, criou o arquivo application-test.properties para gerenciar configurações específicas de teste e expandiu CreateJobControllerTest para conduzir testes do cadastro de um Job

--- src/test/java/br/com/rafaeljaber/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/br/com/rafaeljaber/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -1,6 +1,8 @@
 package br.com.rafaeljaber.gestao_vagas.modules.company.controllers;
 
 import br.com.rafaeljaber.gestao_vagas.modules.company.dto.CreateJobDTO;
+import br.com.rafaeljaber.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.rafaeljaber.gestao_vagas.modules.company.repositories.ICompanyRepository;
 import br.com.rafaeljaber.gestao_vagas.utils.TestUtils;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -20,12 +23,16 @@ import java.util.UUID;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class CreateJobControllerTest {
 
     private MockMvc mvc;
 
     @Autowired
     private WebApplicationContext context;
+
+    @Autowired
+    private ICompanyRepository companyRepository;
 
     @Before
     public void setup() {
@@ -38,6 +45,15 @@ public class CreateJobControllerTest {
     @Test
     public void should_be_able_to_create_a_new_job() throws Exception {
 
+        CompanyEntity company = CompanyEntity.builder()
+                .description("COMPANY_TEST")
+                .email("COMPANY@COMPANY.COM")
+                .password("COMPANY12345")
+                .name("COMPANY_NAME")
+                .username("COMPANY_USERNAME")
+                .build();
+        companyRepository.saveAndFlush(company);
+
         CreateJobDTO createJobDTO = CreateJobDTO.builder()
                 .benefits("BENEFITS_TEST")
                 .description("DESCRIPTION_TEST")
@@ -47,7 +63,7 @@ public class CreateJobControllerTest {
         var result = mvc.perform(MockMvcRequestBuilders.post("/company/job/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(TestUtils.objectToJson(createJobDTO))
-                .header("Authorization", TestUtils.generateToken(UUID.fromString("feebceef-ec2f-4b32-b3e1-67c1233c60a6"), "JAVAGAS_@123#"))
+                .header("Authorization", TestUtils.generateToken(UUID.fromString(company.getId().toString()), "JAVAGAS_@123#"))
         )
                 .andExpect(MockMvcResultMatchers.status().isOk());
 
